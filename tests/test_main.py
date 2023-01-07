3ca2b636d286ea2cdfdd45f68e401642e891d20b
danielalazzara <97309237+danielalazzara@users.noreply.github.com>
2023-01-07 17:19:20 +0000

    More api/team tests
    Co-authored-by: danielalazzara <97309237+danielalazzara@users.noreply.github.com>
    Co-authored-by: markgreene74 <markgreene74@users.noreply.github.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,10 @@ import main
 
 client = TestClient(main.app)
 
+test_input_1 = "UAA%20Aroso"
+test_expected_1 = [{"team":"UAA Aroso","stats_data":{"total_points":486,"mean_points":32.4,"maximum_point":52,"minimum_point":18,"games_played":15,"wins":3,"wins_percentage":0.2}}]
+test_input_2 = "Maia%20Basket"
+test_expected_2 = [{"team":"Maia Basket","stats_data":{"total_points":1142,"mean_points":63.44444444444444,"maximum_point":99,"minimum_point":30,"games_played":18,"wins":17,"wins_percentage":0.9444444444444444}},{"team":"Maia Basket B","stats_data":{"total_points":498,"mean_points":38.30769230769231,"maximum_point":68,"minimum_point":16,"games_played":13,"wins":4,"wins_percentage":0.3076923076923077}}]
 
 def test_read_root():
     response = client.get("/")
@@ -18,8 +22,8 @@ def test_return_team_not_exist():
     assert response.json() == {"detail":"Team abcd not found."}
 
 
-def test_return_team_exist():
-    response = client.get("/api/team/UAA%20Aroso")
+@pytest.mark.parametrize("test_input, expected", [(test_input_1, test_expected_1), (test_input_2, test_expected_2)])
+def test_return_team_exist(test_input, expected):
+    response = client.get(f"/api/team/{test_input}")
     assert response.status_code == 200
-    assert response.json() == {"result":[{"team":"UAA Aroso","stats_data":{"total_points":486,"mean_points":32.4,"maximum_point":52,"minimum_point":18,"games_played":15,"wins":3,"wins_percentage":0.2}}]}
-
+    assert response.json() == {"result": expected}
